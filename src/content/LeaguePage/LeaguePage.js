96b07ede231f935a041678f9905064217298d9af
Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
2023-09-17 16:16:30 -0400

    rank movements
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>

--- src/content/LeaguePage/LeaguePage.js
+++ src/content/LeaguePage/LeaguePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CaretUp, CaretDown, Subtract } from '@carbon/icons-react';
 import {
   Theme,
   Grid,
@@ -17,18 +18,15 @@ import {
 
 const config = require('../../config.json');
 const leaderboardHeaders = [
-  { key: 'move', header: '' },
+  { key: 'move', header: 'POS' },
   { key: 'name', header: 'Name' },
   { key: 'h2h', header: 'H2H' },
   { key: 'gw_points', header: 'GW' },
   { key: 'total_points', header: 'Points' },
 ];
 
-// const getCurrentPosition = (id, league_data) => {};
-// const getPreviousPosition = (id, league_data) => {};
-
 const monoMalinyo = (points, id, event) =>
-  event === 1 && id === 343164 ? points + 10 : points;
+  event === 1 && id === 343164 ? points + 13 : points;
 
 const getPointsArrayFromID = (id, league_data) => {
   var points_array = [];
@@ -54,15 +52,19 @@ const getPointsArrayFromID = (id, league_data) => {
   return points_array;
 };
 
-const getTotalPoints = (id, league_data) =>
-  getPointsArrayFromID(id, league_data).reduce(
-    (total, item) => total + item.points,
-    0
-  );
+const getTotalPoints = (id, league_data, gameweek) => {
+  var all_points_array = getPointsArrayFromID(id, league_data);
 
-const getGameweekPoints = (id, league_data) => {
+  if (gameweek) all_points_array = all_points_array.slice(0, gameweek);
+
+  return all_points_array.reduce((total, item) => total + item.points, 0);
+};
+
+const getGameweekPoints = (id, league_data, gameweek) => {
   const points_array = getPointsArrayFromID(id, league_data);
-  return points_array.length ? points_array[points_array.length - 1].points : 0;
+  const idx = gameweek ? gameweek - 1 : points_array.length - 1;
+
+  return idx < points_array.length ? points_array[idx].points : 0;
 };
 
 const getTeamNameFromID = (id, league_data) =>
@@ -80,8 +82,57 @@ const getCurrentGW = league_data => {
 
 const getCurrentGWStatus = league_data => {
   var cache = league_data.matches.findLast(match => match.started);
-  console.log(444, cache && cache.finished);
   return cache && cache.finished;
+};
+
+const getMoveObject = (id, league_data) => {
+  const new_rank = getPosition(id, league_data, getCurrentGW(league_data));
+  const old_rank = getPosition(id, league_data, getCurrentGW(league_data) - 1);
+
+  if (new_rank < old_rank) {
+    return (
+      <>
+        <CaretUp className="move-object" style={{ color: '#00ff00' }} />{' '}
+        {new_rank}
+      </>
+    );
+  } else if (new_rank > old_rank) {
+    return (
+      <>
+        <CaretDown className="move-object" style={{ color: '#ff0000' }} />{' '}
+        {new_rank}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Subtract className="move-object" style={{ color: '#636363' }} />{' '}
+        {new_rank}
+      </>
+    );
+  }
+};
+
+const getPosition = (id, league_data, gameweek) => {
+  const all_players_points_array = league_data.league_entries
+    .filter(entry => entry.entry_name)
+    .map(entry => {
+      return {
+        id: entry.id,
+        gw_points: getGameweekPoints(entry.id, league_data, gameweek),
+        total_points: getTotalPoints(entry.id, league_data, gameweek),
+      };
+    });
+
+  all_players_points_array.sort(function(a, b) {
+    return b.total_points !== a.total_points
+      ? b.total_points - a.total_points
+      : b.gw_points - a.gw_points;
+  });
+
+  const position =
+    all_players_points_array.findIndex(item => item.id === id) + 1;
+  return position;
 };
 
 class LeaguePage extends React.Component {
@@ -155,42 +206,46 @@ class LeaguePage extends React.Component {
   render() {
     var rows = !this.state.league_data
       ? []
-      : this.state.league_data.standings.map(row => ({
-          ...row,
-          id: row.league_entry,
-          move: <></>,
-          name: (
-            <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
-              <div>
-                {getTeamNameFromID(row.league_entry, this.state.league_data)}
+      : this.state.league_data.standings
+          .filter(item =>
+            getTeamNameFromID(item.league_entry, this.state.league_data)
+          )
+          .map(row => ({
+            ...row,
+            id: row.league_entry.toString(),
+            move: getMoveObject(row.league_entry, this.state.league_data),
+            name: (
+              <div style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+                <div>
+                  {getTeamNameFromID(row.league_entry, this.state.league_data)}
+                </div>
+                <div
+                  style={{
+                    marginTop: '5px',
+                    fontSize: 'smaller',
+                    color: 'gray',
+                  }}>
+                  {getPlayerNameFromID(
+                    row.league_entry,
+                    this.state.league_data
+                  )}
+                </div>
               </div>
-              <div
-                style={{
-                  marginTop: '5px',
-                  fontSize: 'smaller',
-                  color: 'gray',
-                }}>
-                {getPlayerNameFromID(row.league_entry, this.state.league_data)}
-              </div>
-            </div>
-          ),
-          h2h: row.total,
-          gw_points: getGameweekPoints(
-            row.league_entry,
-            this.state.league_data
-          ),
-          total_points: getTotalPoints(
-            row.league_entry,
-            this.state.league_data
-          ),
-        }));
-
-    rows = rows.filter(item =>
-      getTeamNameFromID(item.id, this.state.league_data)
-    );
+            ),
+            h2h: row.total,
+            position: getPosition(row.league_entry, this.state.league_data),
+            gw_points: getGameweekPoints(
+              row.league_entry,
+              this.state.league_data
+            ),
+            total_points: getTotalPoints(
+              row.league_entry,
+              this.state.league_data
+            ),
+          }));
 
     rows.sort(function(a, b) {
-      return b.Points - a.Points;
+      return b.total_points - a.total_points;
     });
 
     return (
